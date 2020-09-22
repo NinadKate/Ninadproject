41faa79c0d1b88fe66b687b6bf8338683f347ec7
NinadKate <70950519+NinadKate@users.noreply.github.com>
2020-09-22 10:27:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, Fragment } from 'react'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
+import UserTable from './tables/UserTable'
+
+const App = () => {
+	// Data
+	const usersData = [
+		{ id: 1, name: 'Ninad', username: 'Ninad@1997',number: 7756052722 },
+		{ id: 2, name: 'Sam', username: 'Sam@1992' ,number: 22222222232 },
+		{ id: 3, name: 'Ravi', username: 'ravi@1995' ,number: 2222222333},
+	]
+
+	const initialFormState = { id: null, name: '', username: '',number: '' }
+
+	// Setting state
+	const [ users, setUsers ] = useState(usersData)
+	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	const [ editing, setEditing ] = useState(false)
+
+	// CRUD operations
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setEditing(false)
+
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false)
+
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditing(true)
+
+		setCurrentUser({ id: user.id, name: user.name, username: user.username, number: user.number })
+	}
+
+	return (
+		<div className="container">
+			<h1>IBM data</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{editing ? (
+						<Fragment>
+							<h2>Edit user</h2>
+							<EditUserForm
+								editing={editing}
+								setEditing={setEditing}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+						</Fragment>
+					) : (
+						<Fragment>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</Fragment>
+					)}
+				</div>
+				<div className="flex-large">
+					<h2>Users</h2>
+					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
